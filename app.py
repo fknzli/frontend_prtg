71fd42d008f72772fda9b663f455a73d6f69b802
fkuenzli <fkuenzli@itris.ch>
2022-05-16 13:31:17 +0200

    changes app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import requests
 import json
+import os
 #import pandas as pd
 
 app = Flask(__name__)
@@ -31,9 +32,21 @@ def home():
     data = json.dumps(data)
     #response_df = pd.DataFrame(data=data, columns=['device', 'sensor', 'status'])
     #response_df.head(10)
-    print(response)
     deganzihtmlcode = """<html><head><h1>titel</h1></head></html>""" + data
     return(deganzihtmlcode)
     
     #out += "\n" + x
     return "Hello \n " + out
+
+@app.route("/", methods=['GET'])
+def button_pause():
+    row = ""
+    objid = ""
+    uri = ""
+    prtgcore = "https://az999-vmappl02.itris-cloud.ch"
+    for row in data:
+        objid = row.Cells[0].Value
+        uri = prtgcore+"'/api/pause.htm?id='objid'&pausemsg='message - Paused by USERNAME'&action=0&'auth"
+        export = export.get(uri)
+
+
