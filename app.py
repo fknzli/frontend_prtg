60ffb38748993c6c881f90e28bbab8478a6ba243
fkuenzli <fkuenzli@itris.ch>
2022-05-19 14:59:00 +0200

    remove libraries

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from http.client import responses
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, request
 import requests
 import json
 
